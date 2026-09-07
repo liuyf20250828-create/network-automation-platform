da5f518c8db58b71900435864d004acea4c396fa
liuyf20250828-create <liuyf20250828@gmail.com>
2026-09-07 20:53:05 +0800

    Update connection.py

--- config/src/connection.py
+++ config/src/connection.py
@@ -1,1 +1,10 @@
+from netmiko import ConnectHandler
 
+def connect_device(device):
+    connection = ConnectHandler (
+      device_type = device["device_type"],
+      host = device["host"],
+      username = device["username"],
+      password = device["password"],
+    )
+  return connection
