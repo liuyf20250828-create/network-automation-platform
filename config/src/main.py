15162f4393277e6dc192c31a0153f913d9d5bef3
liuyf20250828-create <liuyf20250828@gmail.com>
2026-09-07 21:00:14 +0800

    Update main.py

--- config/src/main.py
+++ config/src/main.py
@@ -1,1 +1,26 @@
+Python
 
+import yaml
+from rich import print
+
+from connection import connect_device
+
+def load_devices():
+      with open("config/devices.yaml","r",encoding="utf-8") as f:
+        return yaml.safe_load(f)["devices"]
+
+def main():
+    devices = load_devices()
+
+    for device in devices:
+        print(f"\n[bold cyan]Connection to {device['name']}...[/bold cyan]")
+        try:
+          conn = connect_device(devices)
+          hostname = conn.sent_command("display version")
+          print("[green]Connection successful[/green]")
+          print(hostname[:500])
+          conn.disconnect()
+        except Exception as e:
+          print(f"[red]Connection failed:{e}[/red]")
+if __name__ == "__main__":
+  main()
